b61c787ed0cd7f6ab3bcd5d4dcdc6a82ace4f806
Ayush Singh <asayushsingh80@gmail.com>
2025-01-23 01:58:08 +0530

    basic home screem layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useTheme } from "./providers/theme-provider";
-
 function App() {
-  const { setTheme } = useTheme();
-  return <div onClick={() => setTheme("dark")}>hello</div>;
+  return (
+    <main className="flex flex-col h-screen justify-center items-center gap-10">
+      <h1 className="text-4xl">AskMe!</h1>
+      <div>input + button</div>
+    </main>
+  );
 }
 
 export default App;
