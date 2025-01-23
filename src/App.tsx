c4c8d67454afcabca4ebbb8ec95e256b198586b7
Ayush Singh <asayushsingh80@gmail.com>
2025-01-24 02:42:48 +0530

    ui: homescreen layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import { Input } from "./components/ui/input";
+import { Textarea } from "./components/ui/textarea";
+import { Button } from "./components/ui/button";
+import { ChevronRight } from "lucide-react";
+
 function App() {
+  const [input, setInput] = useState("");
+
   return (
     <main className="flex flex-col h-screen justify-center items-center gap-10">
-      <h1 className="text-4xl">AskMe!</h1>
-      <div>input + button</div>
+      <div className="flex items-center gap-1">
+        <h1 className="text-8xl font-aftersmile">Ask</h1>
+        <span className="text-8xl font-aftersmile logo-background text-transparent">
+          Me?
+        </span>
+      </div>
+      <div className="w-3/4 flex flex-col gap-8">
+        <Textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="w-full"
+          placeholder="Enter a topic or generate from context!"
+        />
+        <Button className="self-center rounded-full">
+          <ChevronRight size={80} />
+        </Button>
+      </div>
     </main>
   );
 }
